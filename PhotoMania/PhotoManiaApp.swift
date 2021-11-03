1d60016b324587e89781b6b324e6a3cd10b004cd
Christopher Delaney <cpd47@georgetown.edu>
2021-11-03 14:04:07 -0400

    Initial Commit

---
+++ PhotoMania/PhotoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiaApp.swift
+//  PhotoMania
+//
+//  Created by Christopher Delaney on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
