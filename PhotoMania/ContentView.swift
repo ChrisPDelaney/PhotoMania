d2e1085013956832d254042a9a43ffd9a40f11c8
Christopher Delaney <cpd47@georgetown.edu>
2021-11-03 15:18:02 -0400

    Image generating and button functionality implemented.

--- PhotoMania/ContentView.swift
+++ PhotoMania/ContentView.swift
@@ -7,19 +7,71 @@
 
 import SwiftUI
 
+class ViewModel: ObservableObject{
+    @Published var image: Image?
+
+    func fetchNewImage(){
+        print("hello")
+        guard let url = URL(string:"http://random.imagecdn.app/500/500")else{
+            return
+        }
+        //testing hjhj
+        //retrieves the data
+        let task = URLSession.shared.dataTask(with: url){ data, _, _ in
+            guard let data = data else {return}
+            
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data) else{
+                    print("not loading")
+                    return
+                }
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         NavigationView{
             VStack{
-                Image(systemName: "photo")
-                    .resizable()
-                    .foregroundColor(Color.pink)
-                    .frame(width: 300, height: 300)
+                
+                Spacer()
+                
+                if let image = viewModel.image {
+                    image
+                        .resizable()
+                        .foregroundColor(Color.pink)
+                        .frame(width: 200, height: 200)
+                        .padding()
+                }else{
+                    Image(systemName: "photo")
+                        .resizable()
+                        .foregroundColor(Color.pink)
+                        .frame(width: 200, height: 200)
+                        .padding()
+                }
+                
+                //Image we are going to randomly generate
+            
+                
+                //Space between our image and button
+                Spacer()
                 
                 Button(action: {
-                    
+                    viewModel.fetchNewImage()
                 },label:{
                     Text("New Image")
+                        .bold()
+                        .frame(width: 250, height: 40)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding()
                 })
                 
                 
